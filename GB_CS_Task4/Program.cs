6318b249b064e2c9683afd2d11375617283af016
Rustavelli <hec8832@gmail.com>
2023-07-06 14:21:31 +0300

    GB_CS_Task4.Done

---
+++ GB_CS_Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("А теперь давайте поиграем с числами и сравним их!");
+
+Console.Write("Задайте первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте первое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте первое число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Ваше первое число больше остальных {num1}, {num2}, {num3} -> *{num1}*");
+}
+else if (num2 > num3)
+{
+    Console.WriteLine($"ваше второе число больше остальных {num1}, {num2}, {num3} -> *{num2}*");
+}
+else if (num3 > num1)
+{
+    Console.WriteLine($"Ваше третье число больше остальных {num1}, {num2}, {num3} -> *{num3}*");
+}
